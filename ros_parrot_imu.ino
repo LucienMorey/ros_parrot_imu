03ff7c868266404d0852d4a11ee4f625f9ed6d62
Lucien Morey <lucien@spacemachines.co>
2023-02-18 08:20:20 +1100

    cleanup

--- ros_parrot_imu.ino
+++ ros_parrot_imu.ino
@@ -4,8 +4,6 @@
 #include "SPI.h"
 #include "constants.h"
 
-
-
 #include <stdio.h>
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
@@ -16,11 +14,9 @@
 
 rcl_publisher_t publisher;
 sensor_msgs__msg__Imu msg;
-// rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-// rcl_timer_t timer;
 
 #define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
 #define LED_PIN 13
@@ -71,11 +67,8 @@ RCCHECK(rclc_publisher_init_default(
     // If the DMP is enabled, .begin performs a minimal startup. We need to configure the sample mode etc. manually.
     myICM.begin(CS_PIN, SPI);
 
-    // Serial.print(F("Initialization of the sensor returned: "));
-    // Serial.println(myICM.statusString());
     if (myICM.status != ICM_20948_Stat_Ok)
     {
-      // Serial.println(F("Trying again..."));
       delay(500);
     }
     else
@@ -83,8 +76,6 @@ RCCHECK(rclc_publisher_init_default(
       initialized = true;
     }
   }
-
-  // Serial.println(F("Device connected!"));
 
   bool success = true;
 
@@ -123,14 +114,16 @@ RCCHECK(rclc_publisher_init_default(
 
   if (success)
   {
+    // TODO re-enable after switching to udp transport
     // Serial.println(F("DMP enabled!"));
   }
   else
   {
+    // TODO re-enable after switching to udp transport
     // Serial.println(F("Enable DMP failed!"));
     // Serial.println(F("Please check that you have uncommented line 29 (#define ICM_20948_USE_DMP) in ICM_20948_C.h..."));
     while (1)
-      ; // Do nothing more
+      error_loop();
   }
 
   memset(&msg, 0, sizeof(sensor_msgs__msg__Imu));
@@ -138,7 +131,6 @@ RCCHECK(rclc_publisher_init_default(
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
   icm_20948_DMP_data_t data;
   myICM.readDMPdataFromFIFO(&data);
@@ -150,76 +142,32 @@ void loop() {
       // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
       // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
       // The quaternion data is scaled by 2^30.
-
-      //SERIAL_PORT.printf("Quat9 data is: Q1:%ld Q2:%ld Q3:%ld Accuracy:%d\r\n", data.Quat9.Data.Q1, data.Quat9.Data.Q2, data.Quat9.Data.Q3, data.Quat9.Data.Accuracy);
-
       // Scale to +/- 1
+
       msg.orientation.x = ((double)data.Quat9.Data.Q1) * ORIENTATION_CONVERSION_FACTORS::QUATERNION_9_DOF; // Convert to double. Divide by 2^30
       msg.orientation.y = ((double)data.Quat9.Data.Q2) * ORIENTATION_CONVERSION_FACTORS::QUATERNION_9_DOF; // Convert to double. Divide by 2^30
       msg.orientation.z = ((double)data.Quat9.Data.Q3) * ORIENTATION_CONVERSION_FACTORS::QUATERNION_9_DOF; // Convert to double. Divide by 2^30
       msg.orientation.w = sqrt(1.0 - ((msg.orientation.x * msg.orientation.x) + (msg.orientation.y * msg.orientation.y) + (msg.orientation.z * msg.orientation.z)));
 
-      // Serial.print(F("Q1:"));
-      // Serial.print(msg.orientation.w, 3);
-      // Serial.print(F(" Q2:"));
-      // Serial.print(msg.orientation.x, 3);
-      // Serial.print(F(" Q3:"));
-      // Serial.print(msg.orientation.y, 3);
-      // Serial.print(F(" Accuracy:"));
-      // Serial.println(data.Quat9.Data.Accuracy);
-
     }
 
-    if ((data.header & DMP_header_bitmap_Accel) > 0) // We have asked for orientation data so we should receive Quat9
+    if ((data.header & DMP_header_bitmap_Accel) > 0) // We have asked for acceleration so we should receive raw accel
     {
       msg.linear_acceleration.x = (float)data.Raw_Accel.Data.X * ACCEL_CONVERSION_FACTORS::ACEEL_8G * GRAVITY;
       msg.linear_acceleration.y = (float)data.Raw_Accel.Data.Y * ACCEL_CONVERSION_FACTORS::ACEEL_8G * GRAVITY;
       msg.linear_acceleration.z = (float)data.Raw_Accel.Data.Z * ACCEL_CONVERSION_FACTORS::ACEEL_8G * GRAVITY;
 
-      // Serial.print(F("{\"accel_x\":"));
-      // Serial.print(msg.linear_acceleration.x, 3);
-      // Serial.print(F(", \"accel_y\":"));
-      // Serial.print(msg.linear_acceleration.y, 3);
-      // Serial.print(F(", \"accel_z\":"));
-      // Serial.print(msg.linear_acceleration.z, 3);
-      // Serial.println(F("}"));
-
     }
 
-    if ((data.header & DMP_header_bitmap_Gyro) > 0) // We have asked for orientation data so we should receive Quat9
+    if ((data.header & DMP_header_bitmap_Gyro) > 0) // We have asked angular velocity so we should receive raw gyro
     {
       msg.angular_velocity.x = (float)data.Raw_Gyro.Data.X * GYRO_CONVERSION_FACTORS::GYRO_250_DPS * PI/180.0;
       msg.angular_velocity.y = (float)data.Raw_Gyro.Data.Y * GYRO_CONVERSION_FACTORS::GYRO_250_DPS * PI/180.0;
       msg.angular_velocity.z = (float)data.Raw_Gyro.Data.Z * GYRO_CONVERSION_FACTORS::GYRO_250_DPS * PI/180.0;
 
-      // Serial.print(F("{\"gyro_x\":"));
-      // Serial.print(msg.angular_velocity.x, 3);
-      // Serial.print(F(", \"gyro_y\":"));
-      // Serial.print(msg.angular_velocity.y, 3);
-      // Serial.print(F(", \"gyro_z\":"));
-      // Serial.print(msg.angular_velocity.z, 3);
-      // Serial.println(F("}"));
-
     }
   }
 
-  // if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
-  // {
-  //   delay(10);
-  // }
-  int32_t temp = 0;
-  myICM.getBiasAccelX(&temp);
-  msg.linear_acceleration_covariance[0] = temp;
-  myICM.getBiasAccelY(&temp);
-  msg.linear_acceleration_covariance[1] = temp;
-  myICM.getBiasAccelZ(&temp);
-  msg.linear_acceleration_covariance[2] = temp;
-  myICM.getBiasGyroX(&temp);
-  msg.linear_acceleration_covariance[3] = temp;
-  myICM.getBiasGyroY(&temp);
-  msg.linear_acceleration_covariance[4] = temp;
-  myICM.getBiasGyroZ(&temp);
-  msg.linear_acceleration_covariance[5] = temp;
 
   RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
 
